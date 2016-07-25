6848f0e423849378f25901ec3cf547edef43805f
hands-rec <hands.records@gmail.com>
2016-07-25 21:38:34 +0900

    Implement

--- lib/studio_home/crawler.rb
+++ lib/studio_home/crawler.rb
@@ -1,7 +1,149 @@
 require "studio_home/crawler/version"
 
+require 'nokogiri'
+require 'date'
+require 'open-uri'
+require 'pp'
+
 module StudioHome
   module Crawler
-    # Your code goes here...
+
+    class Client
+      MINIMUM_SLEEP = 1
+      CRAWLER_UA = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; Trident/7.0; rv:11.0) like Gecko'
+      attr_reader :site_url
+      def initialize(place, sleep=3)
+        @place = place
+        @sleep = MINIMUM_SLEEP < sleep ? sleep : MINIMUM_SLEEP
+        @site_url = "https://www3.revn.jp/studio-home/?c_type=#{c_type}&date="
+      end
+
+      def find_bookable_list(weeks: 4)
+        list = []
+        date = Date.today
+        weeks.times do |i|
+          sleep @sleep
+          base_date = date + (i * 7)
+          url = @site_url + base_date.strftime("%Y-%m-%d")
+          pp url
+          source = open(url, 'User-Agent' => CRAWLER_UA)
+           
+          scraper = StudioHome::Crawler::Scraper.new(source)
+          list += scraper.find_bookable_list
+        end
+        list
+      end
+
+      private
+      
+      def c_type
+        if @place == '鎌倉雪ノ下' 
+          1
+        elsif @place == '横須賀西海岸店'
+          4
+        else
+          raise ArgumentError, "bad argument"
+        end
+      end
+
+    end
+
+    class Scraper
+      attr_accessor :schedule
+      def initialize(source)
+        @schedule = Nokogiri::HTML(source).css('#Schedule')
+        @date_time_list = []
+      end
+
+      def execute
+        7.times do |i|
+          num = i + 1
+          date = scrape_date(num)
+          6.times do |j|
+            time_id = j + 1
+            value = scrape_value(num, time_id)
+            @date_time_list << ReservationDateTime.new(date, time_id, value)
+          end
+        end
+        @date_time_list
+      end
+
+      def find_bookable_list
+        execute if @date_time_list.length == 0
+        list = []
+        @date_time_list.each do |datetime|
+          list << datetime if datetime.available?
+        end
+        list
+      end
+
+      def scrape_date(num)
+        num = num + 1
+        date = @schedule.css("tr:nth-child(2) td.Time:nth-child(#{num})")
+        strip(date.text)
+      end
+
+      def scrape_value(num, time_id)
+        value = scrape_value_cell(num, time_id)
+        strip(value.text)
+      end
+
+      def scrape_link(num, time_id)
+        link = scrape_value_cell(num, time_id)
+        link.css('a').attr('href').value
+      end
+
+      private
+
+      def scrape_value_cell(row, col)
+        row = 1 + row
+        col = 2 + col
+        value = @schedule.css("tr:nth-child(#{col}) td:nth-child(#{row})")
+      end
+      
+      def strip(str)
+        str.gsub("\n", "").strip.gsub(" ", "").gsub(" ", "").gsub(" ", "")
+      end
+    end
+
+    class ReservationDateTime
+      attr_accessor :date, :time_id, :value, :link
+      
+      def initialize(date, time_id, value)
+        @date = date
+        @time_id = time_id
+        @value = value
+        @link = link
+      end
+
+      def available?
+        @value == "○"
+      end
+
+      def to_s
+        "#{date} #{time}"
+      end
+
+      def time
+        if @time_id == 1
+          "08時30分 ～ 10時30分"
+        elsif @time_id == 2
+          "10時00分 ～ 12時00分"
+        elsif @time_id == 3
+          "11時30分 ～ 13時30分"
+        elsif @time_id == 4
+          "13時00分 ～ 15時00分"
+        elsif @time_id == 5
+          "14時30分 ～ 16時30分"
+        elsif @time_id == 6
+          "15時30分 ～ 17時30分"
+        end
+      end
+    end
+
+    class Aa
+      def initialize
+      end
+    end
   end
 end
--- spec/studio_home/crawler_spec.rb
+++ spec/studio_home/crawler_spec.rb
@@ -1,11 +1,127 @@
 require 'spec_helper'
+require 'pp'
 
 describe StudioHome::Crawler do
   it 'has a version number' do
     expect(StudioHome::Crawler::VERSION).not_to be nil
   end
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+describe StudioHome::Crawler::Client do
+  describe '#find_bookable_list' do
+    it 'success' do
+#      client = StudioHome::Crawler::Client.new('鎌倉雪ノ下')
+#      list = client.find_bookable_list(weeks: 3)
+#      pp list
+    end
+  end
+
+  describe 'initialize' do
+    it '鎌倉雪ノ下' do
+      client = StudioHome::Crawler::Client.new('鎌倉雪ノ下')
+      expect(client.site_url).to match 'c_type=1'
+    end
+    it '横須賀西海岸店' do
+      client = StudioHome::Crawler::Client.new('横須賀西海岸店')
+      expect(client.site_url).to match 'c_type=4'
+    end
+#    it 'bad argument' do
+#      client = StudioHome::Crawler::Client.new('')
+#    end
+  end
+end
+
+describe StudioHome::Crawler::Scraper do
+  before :each do
+    doc = open('./test/studio_home.htm')
+    @scraper = StudioHome::Crawler::Scraper.new(doc)
+  end
+
+  describe '#initialize' do
+    it 'has a schedule' do
+      expect(@scraper.schedule.attr('id').value).to eq 'Schedule'
+    end
+  end
+
+  describe '#scrape_date' do
+    it 'first' do
+      date = @scraper.scrape_date(1)
+      expect(date).to eq '7/13(水)'
+    end
+  end
+  
+  describe '#scrape_value' do
+    it '予約期間外' do
+      value = @scraper.scrape_value(1, 1)
+      expect(value).to eq '予約期間外'
+    end
+
+    it '定休日' do
+      value = @scraper.scrape_value(2, 1)
+      expect(value).to eq '<<定休日>>'
+    end
+
+    it '×' do
+      value = @scraper.scrape_value(3, 1)
+      expect(value).to eq '×'
+    end
+
+    it '○' do
+      value = @scraper.scrape_value(3, 5)
+      expect(value).to eq '○'
+    end
+
+    it 'empty' do
+      value = @scraper.scrape_value(4, 6)
+      expect(value).to eq ''
+    end
+  end
+
+  describe '#scrape_link' do
+    it 'exist' do
+      link = @scraper.scrape_link(3, 5)
+      expect(link).to eq 'https://www3.revn.jp/studio-home/yoyaku/form?f_tid=15407&symbol=E'
+    end
+  end
+
+  describe '#execute' do
+    it 'count' do
+      list = @scraper.execute
+      expect(list.length).to eq 42 
+    end
+  end
+
+  describe '#find_bookable_list' do
+    before :each do
+      @list = @scraper.find_bookable_list
+    end
+
+    it 'count' do
+      expect(@list.length).to eq 1 
+    end
+
+    it 'content' do
+      expect(@list.first.date).to eq '7/15(金)'
+    end
+  end
+end
+
+describe StudioHome::Crawler::ReservationDateTime do
+  describe '#available?' do
+    it 'false' do
+      datetime = StudioHome::Crawler::ReservationDateTime.new(nil, nil, '予約期間外')
+      expect(datetime.available?).to be false
+    end
+    it 'true' do
+      datetime = StudioHome::Crawler::ReservationDateTime.new(nil, nil, '○')
+      expect(datetime.available?).to be true
+    end
+  end
+
+  describe '#to_s' do
+    it 'exist time_id' do
+      datetime = StudioHome::Crawler::ReservationDateTime.new('7/13 (水)', 1, '○')
+      expect(datetime.to_s).to eq '7/13 (水) 08時30分 ～ 10時30分'
+    end
   end
 end
